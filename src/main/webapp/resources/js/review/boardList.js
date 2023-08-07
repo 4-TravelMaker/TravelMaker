f49ba5320f886d2358150b6c599dde846ba735d2
ash <dkstngus0615@gmail.com>
2023-08-07 16:07:39 +0900

    0807 최종 커밋

---
+++ src/main/webapp/resources/js/review/boardList.js
@@ -0,0 +1,21 @@
+// 게시글 최신순 정렬
+
+const SortByRecencyBtn = document.getElementById("SortByRecency-btn");
+
+SortByRecencyBtn.addEventListener("click", function(){
+
+    $.ajax({
+
+        url : "SortByRecency",
+
+        data : {}
+
+
+
+
+
+
+    })
+
+
+})
